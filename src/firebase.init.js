8d073551dcb895ec333806453c378fae5c6eb773
nazmul162001 <nazmulprime162001@gmail.com>
2022-04-08 23:03:29 +0600

    project setUP & folder structure complete

---
+++ src/firebase.init.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAd35wkYLtcvTUvppEI2bn6I2x0si2Hom4",
+  authDomain: "fir-router-integration-9cf8b.firebaseapp.com",
+  projectId: "fir-router-integration-9cf8b",
+  storageBucket: "fir-router-integration-9cf8b.appspot.com",
+  messagingSenderId: "648099596810",
+  appId: "1:648099596810:web:ef437d88290458b86dedf3"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
