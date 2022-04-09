fdd6c3a338e6a4e69152c9b71122b2ba0b9e9278
nazmul162001 <nazmulprime162001@gmail.com>
2022-04-09 15:34:24 +0600

    added privet route

--- src/App.js
+++ src/App.js
@@ -5,16 +5,26 @@ import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import Products from './components/Products/Products';
 import Register from './components/Register/Register';
+import Orders from './components/Orders/Orders';
+import RequireAuth from './components/RequireAuth/RequireAuth';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Routes>
-        <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/login' element={<Login></Login>}></Route>
-        <Route path='/register' element={<Register></Register>}></Route>
-        <Route path='/products' element={<Products></Products>}></Route>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/register" element={<Register></Register>}></Route>
+        <Route path="/products" element={<Products></Products>}></Route>
+        <Route
+          path="/orders"
+          element={
+            <RequireAuth>
+              <Orders></Orders>
+            </RequireAuth>
+          }
+        ></Route>
       </Routes>
     </div>
   );
---
+++ src/components/Orders/Orders.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Order = () => {
+  return (
+    <div>
+      <h2>My Order</h2>
+    </div>
+  );
+};
+
+export default Order;
